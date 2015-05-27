225de7b9f760b06c71e618c8b960262fc0920062
Xi Wei <xwei@utexas.edu>
2015-05-27 11:15:18 -0400

    New Project

---
+++ app/src/main/java/xizz/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,14 @@
+package xizz.nerdlauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class NerdLauncherActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nerd_launcher);
+	}
+}
